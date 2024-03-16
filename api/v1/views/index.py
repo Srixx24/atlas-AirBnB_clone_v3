0cb79633923f0a407f717f34213420e768a73923
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-16 09:56:01 -0700

    Forgot to add the rest of the files

---
+++ api/v1/views/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, Blueprint, jsonify
+app = Flask(__name__)
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    return jsonify({'status': 'OK'})
+
+app.register_blueprint(app_views)
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
