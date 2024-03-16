84ee0601c2872ecf1dee3e595a5f6d8a1811aad9
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-16 09:08:23 -0700

    Adding new methods to storage files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object from database"""
+        if cls or id is None:
+            return None
+        
+        # Perform the database query to retrieve the object
+        obj = self.query_database(cls, id)
+        return obj
+
+    def count(self, cls=None):
+        """method to count the number of objects in storage"""
+        return len(self.all(cls))
+
