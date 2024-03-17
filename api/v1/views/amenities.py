02fe23ef25583c483a3d70284faff2b6ab903607
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-17 12:30:18 -0700

    Adding user file and bones for place and review files

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,7 +11,7 @@ from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def all_amenities():
-    """Gets list of all amenities objects"""
+    """Gets list of all amenity objects"""
     amenities = storage.all(Amenity)
     amenities_list = [amenity.to_dict() for amenity in amenities.values()]
     return jsonify(amenities_list)
@@ -28,7 +28,7 @@ def one_amenity(amenity_id):
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
 def amenity_delete(amenity_id):
-    """Deletes a amenity object"""
+    """Deletes an amenity object"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity:
         storage.delete(amenity)
@@ -39,7 +39,7 @@ def amenity_delete(amenity_id):
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def amenity_create():
-    """Creates a amenity object"""
+    """Creates an amenity object"""
     if not request.json:
         abort(400, 'Not a JSON')
 
@@ -55,9 +55,9 @@ def amenity_create():
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def amenity_update(amenity_id):
-    """Updates a amenity object"""
+    """Updates an amenity object"""
     amenity = storage.get('Amenity', amenity_id)
-    ignored_keys = ["id", "state_id", "created_at", "updated_at"]
+    ignored_keys = ["id", "created_at", "updated_at"]
     if amenity is None:
         abort(404)
     new_city = request.get_json()
