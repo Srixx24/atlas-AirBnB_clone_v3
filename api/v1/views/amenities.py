0ff87d7b7f7c07424cc3481d80cfff0e758366a7
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-17 20:30:08 -0700

    PEP8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -24,7 +24,7 @@ def one_amenity(amenity_id):
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
         abort(404)
-    return jsonify(amenity.to_dict())
+    return jsonify(amenity.to_json())
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,31 +9,36 @@ from models import storage
 from models.place import Place
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
 def all_places():
     """Gets list of all place objects"""
     boop
 
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
 def one_place():
     """Gets the place object"""
     boop
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def place_delete():
     """Deletes a place object"""
     boop
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
 def place_create():
     """Creates a place object"""
     boop
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
 def place_update():
     """Updates a place object"""
     boop
