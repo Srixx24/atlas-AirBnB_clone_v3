5c276eaf455dc27dfd3156dbff7e5f1d9b3d9e73
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-17 18:55:00 -0700

    Trying all this again from states to amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -20,10 +20,10 @@ def all_amenities():
 @app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def one_amenity(amenity_id):
     """Gets the amenity object"""
-    amenity = storage.get(Amenity, amenity_id)
+    amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
         abort(404)
-    return jsonify(amenity.to_dict())
+    return jsonify(amenity.to_dict()), 200
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
