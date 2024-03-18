dedb32cd98e2a9fbe624c62f7ef4be93801dcad5
Greentaylor27 <7196@holbertonstudents.com>
2024-03-18 16:49:19 +0000

    Trying to fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,7 +81,7 @@ class DBStorage:
             return None
 
         # Perform the database query to retrieve the object
-        obj = self.query_database(cls, id)
+        obj = self.__session.query(cls, id)
         return obj
 
     def count(self, cls=None):
